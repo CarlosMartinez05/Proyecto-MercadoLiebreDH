64885ff48577b8898cf30808fae72016429156e6
CarlosMartinez05 <carlitosmartinez0593@gmail.com>
2021-11-03 18:53:50 -0300

    cambios en app.js y en home.html

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 
 const publicPath= path.resolve(__dirname, './public/image')
 
+app.use(express.static(publicPath))
 
 app.listen(3000,()=>console.log('todo salio bien'))
 
@@ -17,6 +18,6 @@ app.listen(3000,()=>console.log('todo salio bien'))
 app.use(express.static('public'))
 
 app.get('/',(req, res)=>{
-    res.sendFile(path.join(__dirname + '/views/home.html'))
+    res.sendFile(path.resolve(__dirname + '/views/home.html'))
 });
 
